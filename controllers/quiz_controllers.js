d46f5ee762782c8fe336e64bee7c6da185342da3
mysoftchile <cmorales@mysoft.cl>
2015-08-03 20:39:58 -0400

    Con Modelo y sqlite
    Se le agregó modelo (sequelize) y base de datos sqlite

--- controllers/quiz_controllers.js
+++ controllers/quiz_controllers.js
@@ -1,24 +1,19 @@
-﻿//get a preguntas
-exports.question = function (req, res) {
-	res.render('quizes/question', { pregunta:'Capital de Italia' });
-};            
+﻿var models = require('../models/models.js');
 
-exports.creditos = function (req, res) {
-    res.render('quizes/author', { credito: 'Página creada por Cristian Morales' });
+// GET /quizes/question
+exports.question = function (req, res) {
+    models.Quiz.findAll().then(function (quiz) {
+        res.render('quizes/question', { pregunta: quiz[0].pregunta });
+    })
 };
 
-//get a respuestas
+// GET /quizes/answer
 exports.answer = function (req, res) {
-	
-	respuesta = req.query.respuesta;
-
-	 if(respuesta === 'Roma')
-	{
-		res.render('quizes/answer', { respuesta: 'Su respuesta:' + respuesta + ', es correcta !!!' });
-	}
-	else
-	{
-		res.render('quizes/answer', { respuesta: 'Su respuesta es Incorrecta' });
-	}
-
+    models.Quiz.findAll().then(function (quiz) {
+        if (req.query.respuesta === quiz[0].respuesta) {
+            res.render('quizes/answer', { respuesta: 'Correcto !!!' });
+        } else {
+            res.render('quizes/answer', { respuesta: 'Incorrecto' });
+        }
+    })
 };
---
+++ models/models.js
@@ -0,0 +1,28 @@
+﻿var path = require('path');
+
+// Carga Modelo ORM
+var Sequelize = require('sequelize');
+
+// Usar BBDD SQLite
+var sequelize = new Sequelize(null, null, null,
+  {dialect: "sqlite", storage: "quiz.sqlite"}
+);
+
+// Importar definicion de la tabla Quiz/ con ruta a definición de la tabla
+var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
+
+exports.Quiz = Quiz //Expotar definición de la tabla quiz
+
+//Crea BD y la inicializa (Al sincronizar el modelo)
+sequelize.sync().then(function ()
+{
+      // success(..) ejecuta el manejador una vez creada la tabla
+      Quiz.count().then(function (count){
+            if(count === 0) {   // la tabla se inicializa solo si está vacía
+                  Quiz.create({ pregunta: 'Capital de Italia',
+                      	            respuesta: 'Roma'
+                      	         })
+              .then(function(){console.log('Base de datos inicializada')});
+            };
+      });
+}); 
