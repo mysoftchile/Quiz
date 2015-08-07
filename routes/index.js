15d885081c04cd6ac4921ed9cebbea6925c51e2e
mysoftchile <cmorales@mysoft.cl>
2015-08-06 23:30:13 -0400

    Con Moderación de comentarios

--- routes/index.js
+++ routes/index.js
@@ -14,6 +14,7 @@ router.get('/', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
+router.param('commentId', commentController.load);  // autoload :commentId
 
 // Definición de rutas de sesion
 router.get('/login',  sessionController.new);     // formulario login
@@ -36,5 +37,5 @@ router.get('/author', creditsController.creditos)
 
 router.get('/quizes/:quizId(\\d+)/comments/new',  commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments',     commentController.create);
-
+router.get('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',  sessionController.loginRequired, commentController.publish);
 module.exports = router;
