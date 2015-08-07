0eb54d5aca32dd0204e5cb2cf9c5dd908cf05cbd
mysoftchile <cmorales@mysoft.cl>
2015-08-06 22:38:39 -0400

    Con Login de usuario

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router();
 var quizController = require('../controllers/quiz_controllers.js')
 var creditsController = require('../controllers/creditscontrollers.js')
 var commentController = require('../controllers/comment_controller');
+var sessionController = require('../controllers/session_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -13,6 +14,11 @@ router.get('/', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);  // autoload :quizId
+
+// Definición de rutas de sesion
+router.get('/login',  sessionController.new);     // formulario login
+router.post('/login', sessionController.create);  // crear sesión
+router.get('/logout', sessionController.destroy); // destruir sesión
 
 
 // Definición de rutas de /quizes
